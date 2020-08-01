2b5b7f3d72ec63f5674626b8b71cc6c5076656dd
juliotmorais <juliotmorais@gmail.com>
2020-08-01 20:48:18 +0900

    allowed for multiple enemy spawn in a wave

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [SerializeField] List<WaveConfig> waveConfigs;
+    int startingWave = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var currentWave = waveConfigs[startingWave];
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveconfig)
+    {
+        for (
+            int enemyCount = 0; 
+            enemyCount <= waveconfig.GetNumberOfEnemies();
+            enemyCount++) 
+        {
+            Instantiate(waveconfig.GetEnemyPrefab(), waveconfig.GetWaypoints()[0].transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(
+                waveconfig.GetTimeBetweenSpawn()
+                );
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
